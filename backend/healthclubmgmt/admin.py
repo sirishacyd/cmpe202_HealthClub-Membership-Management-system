8c012da3022b8f96402710a99efa3e723d0cd537
darshcloud <darshini.vn@gmail.com>
2023-03-16 11:42:24 -0700

    Created Enrollment and Location Models

--- backend/healthclubmgmt/admin.py
+++ backend/healthclubmgmt/admin.py
@@ -1,21 +1,31 @@
 from django.contrib import admin
-from .models import Training,User,User_log,Activity,ActivityLog
-
+from .models import Training, User, User_log, Activity, ActivityLog, Location, Enrollments
 
 
 class TrainingAdmin(admin.ModelAdmin):
-    list_display = ('training_id','training_type','instructor_name','start_time','end_time','max_capacity','current_capacity')
+    list_display = (
+        'training_id', 'training_type', 'instructor_name', 'start_time', 'end_time', 'max_capacity', 'current_capacity')
+
 
 class UserAdmin(admin.ModelAdmin):
-    list_display=('user_id','first_name','last_name','user_type','trial_expiry')
+    list_display = ('user_id', 'first_name', 'last_name', 'user_type', 'trial_expiry')
+
 
 class User_logAdmin(admin.ModelAdmin):
-    list_display=('user_id','checkin_time','checkout_time')
-    
+    list_display = ('user_id', 'checkin_time', 'checkout_time')
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('location_name', 'location_address')
+
+
+class EnrollmentsAdmin(admin.ModelAdmin):
+    list_display = ('training_id', 'user_id')
+
+
 # Register your models here. 
-admin.site.register(Training,TrainingAdmin)
-admin.site.register(User,UserAdmin)
-admin.site.register(User_log,User_logAdmin)
-
-
-
+admin.site.register(Training, TrainingAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(User_log, User_logAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Enrollments, EnrollmentsAdmin)
