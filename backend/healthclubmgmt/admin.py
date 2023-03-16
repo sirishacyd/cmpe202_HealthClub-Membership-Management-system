ab5733b8c626ca06289bd37bccaa63d2ad5f7597
Bhavya Hegde <85700971+hegdebhavya@users.noreply.github.com>
2023-03-15 20:38:57 -0700

    Created trainings model in the database

--- backend/healthclubmgmt/admin.py
+++ backend/healthclubmgmt/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Training
 
-# Register your models here.
+
+class TrainingAdmin(admin.ModelAdmin):
+    list_display = ('training_id','training_type','instructor_name','start_time','end_time','max_capacity')
+    
+# Register your models here. 
+admin.site.register(Training,TrainingAdmin)
--- backend/healthclubmgmt/models.py
+++ backend/healthclubmgmt/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+
+    
+class Training(models.Model):
+    training_id = models.AutoField(primary_key=True)
+    instructor_name = models.CharField(max_length=255)
+    training_type = models.CharField(max_length=255)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    max_capacity = models.PositiveIntegerField()
+    #location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.training_type
