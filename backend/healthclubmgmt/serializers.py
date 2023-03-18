ee04dda79e50dc80e00824392b4d8bfb0fedd09d
sirishacyd <sirishacyd@gmail.com>
2023-03-17 21:52:59 -0700

    Checkin Serializer Code

--- backend/healthclubmgmt/serializers.py
+++ backend/healthclubmgmt/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Training
+from .models import Training,User_log, Location,User
+# from django.contrib.auth.models import User
 
 
 #Training Model Serializer
@@ -7,3 +8,22 @@ class TrainingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Training
         fields = ('id', 'instructor_name', 'training_type', 'start_time', 'end_time', 'max_capacity', 'location', 'current_capacity')
+        
+
+
+class UserLogSerializer(serializers.ModelSerializer):
+    user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    location_id = serializers.PrimaryKeyRelatedField(queryset=Location.objects.all())
+
+    class Meta:
+        model = User_log
+        fields = ('id', 'user_id', 'checkin_time', 'checkout_time', 'location_id')
+
+    def create(self, validated_data):
+        user_id = validated_data.get('user_id')
+        checkin_time = validated_data.get('checkin_time')
+        location_id = validated_data.get('location_id')
+
+        user_log = User_log(user_id=user_id, checkin_time=checkin_time, location_id=location_id)
+        user_log.save()
+        return user_log
--- backend/healthclubmgmt/views.py
+++ backend/healthclubmgmt/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+# from django.contrib.auth.models import User
+from .models import User_log, User
+from .serializers import UserLogSerializer
+
+class UserLogViewSet(viewsets.ModelViewSet):
+    queryset = User_log.objects.all()
+    serializer_class = UserLogSerializer
+
+    @action(detail=False, methods=['post'])
+    def checkin(self, request):
+        serializer = UserLogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
