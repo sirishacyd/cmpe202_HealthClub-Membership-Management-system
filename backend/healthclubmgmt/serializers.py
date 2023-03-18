17c97ae7142e26edd855906ddd62692c6de136a2
Bhavya Hegde <85700971+hegdebhavya@users.noreply.github.com>
2023-03-17 20:11:33 -0700

    Created Training model serializer

--- backend/healthclubmgmt/serializers.py
+++ backend/healthclubmgmt/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Training
 
 
-#Training Serializer
+#Training Model Serializer
 class TrainingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Training
