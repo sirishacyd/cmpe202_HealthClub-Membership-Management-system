49be2303fae53ef5c2fbe671914e359607890364
Bhavya Hegde <85700971+hegdebhavya@users.noreply.github.com>
2023-03-17 20:10:59 -0700

    Created Training model serializer

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'healthclubmgmt'
+    'healthclubmgmt',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
---
+++ backend/healthclubmgmt/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Training
+
+
+#Training Serializer
+class TrainingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Training
+        fields = ('id', 'instructor_name', 'training_type', 'start_time', 'end_time', 'max_capacity', 'location', 'current_capacity')
