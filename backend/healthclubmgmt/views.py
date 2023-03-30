5737b447ec2641903a6d60f6215118e8812fdbb0
Bhavya Hegde <85700971+hegdebhavya@users.noreply.github.com>
2023-03-30 12:58:10 -0700

    api to get trainings by location

--- backend/healthclubmgmt/views.py
+++ backend/healthclubmgmt/views.py
@@ -3,12 +3,12 @@ from django.http.response import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http.response import JsonResponse
-from .models import Training
+from .models import Training, Enrollments
 from .serializers import TrainingSerializer, EnrollmentsSerializer
 from rest_framework import viewsets, status, generics
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from . import models
+from .import models
 from django_filters.rest_framework import DjangoFilterBackend
 # from django.contrib.auth.models import User
 from .models import User_log, User
@@ -78,31 +78,23 @@ class SignupSet(viewsets.ModelViewSet):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class signUpTraining(generics.ListCreateAPIView):
-    queryset = models.Enrollments.objects.all()
-
+class signUpTraining(viewsets.ModelViewSet):
+    queryset = Enrollments.objects.all()
     serializer_class = EnrollmentsSerializer
-    filter_backends = (DjangoFilterBackend,)
-    filter_fields = ('user_id',)
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['user_id']
+    
+    @action(detail=False, methods=['post'])
+    def signupfortraining(self, request, *args, **kwargs):
+        userTrainingObjects = Enrollments.objects.filter(user_id=request.data["user_id"])
+        userTrainingIDs = [i.training_id.training_id for i in userTrainingObjects]
+        if int(request.data["training_id"]) in userTrainingIDs:
+            return Response({'error': 'User is already registered for this training!'}, status=status.HTTP_400_BAD_REQUEST)
 
-    def create(self, request, *args, **kwargs):
-        userTrainingObjects = []
-        userTrainingIDs = []
-        userTrainingObjects = models.Enrollments.objects.filter(user_id=request.data["user_id"])
-        for i in userTrainingObjects:
-            userTrainingIDs.append(i.training_id.training_id)
-        print(userTrainingIDs)
-        if (int(request.data["training_id"]) in userTrainingIDs):
-            print("User ALREADY Registered for training")
-            return Response({'error': 'User is already registered for this training!'},
-                            status=status.HTTP_400_BAD_REQUEST)
-
-        catch_training = models.Training.objects.get(pk=request.data["training_id"])
+        catch_training = Training.objects.get(pk=request.data["training_id"])
+        if catch_training.current_capacity >= catch_training.max_capacity:
+            return Response({'error': 'Capacity for the training session is full, please reach out to a health club employee for further information'}, status=status.HTTP_400_BAD_REQUEST)
         catch_training.current_capacity += 1
-        if catch_training.current_capacity > catch_training.max_capacity:
-            return Response({
-                                'error': 'Capacity for the training session is full, please reachout health club employee for further information'},
-                            status=status.HTTP_400_BAD_REQUEST)
         catch_training.save()
 
         serializer = self.get_serializer(data=request.data)
@@ -110,3 +102,18 @@ class signUpTraining(generics.ListCreateAPIView):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
+class viewTraining(viewsets.ModelViewSet):
+    queryset = Training.objects.all()
+    serializer_class = TrainingSerializer
+    lookup_field = 'pk'
+    @action(detail=False, methods=['get'])
+    def viewtrainingdetails(self, request, pk=None):
+        if pk is not None:
+            training = self.get_object()
+            serializer = self.get_serializer(training)
+            return Response(serializer.data)
+        else:
+            trainings = self.get_queryset()
+            serializer = self.get_serializer(trainings, many=True)
+            return Response(serializer.data)
