a010354aad4a3b70d19e23bc6fa04ae62bfb2ebe
darshcloud <darshini.vn@gmail.com>
2023-04-01 16:32:45 -0700

    Get Location and Locationdetails API changes

--- backend/healthclubmgmt/views.py
+++ backend/healthclubmgmt/views.py
@@ -6,16 +6,16 @@ from .serializers import TrainingSerializer, EnrollmentsSerializer
 from rest_framework import viewsets, status, generics
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from .import models
+from . import models
 from django_filters.rest_framework import DjangoFilterBackend
 # from django.contrib.auth.models import User
 from .serializers import TrainingSerializer
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
-from .models import User_log, User
-from .serializers import UserLogSerializer, UserSerializer
+from .models import User_log, User, Location
+from .serializers import UserLogSerializer, UserSerializer, LocationSerializer
 from rest_framework.authtoken.models import Token
-from rest_framework.permissions import AllowAny,IsAdminUser
+from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework.authtoken.views import ObtainAuthToken
 
 
@@ -23,6 +23,7 @@ from rest_framework.authtoken.views import ObtainAuthToken
 
 class ClassSchedulesListView(APIView):
     permission_classes = [IsAdminUser]
+
     def post(self, request):
         data = request.data
         serializer = TrainingSerializer(data=data)
@@ -38,6 +39,7 @@ class ClassSchedulesListView(APIView):
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class UserLogViewSet(viewsets.ModelViewSet):
     queryset = User_log.objects.all()
     serializer_class = UserLogSerializer
@@ -50,7 +52,7 @@ class UserLogViewSet(viewsets.ModelViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
     @action(detail=False, methods=['put'])
     def checkout(self, request):
         username = request.data.get('username')
@@ -68,11 +70,10 @@ class UserLogViewSet(viewsets.ModelViewSet):
             return Response({'error': 'User has no check-in entry.'}, status=status.HTTP_400_BAD_REQUEST)
 
 
-
 class SignupSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAdminUser] 
+    permission_classes = [IsAdminUser]
 
     @action(detail=False, methods=['post'])
     def signup(self, request):
@@ -80,25 +81,29 @@ class SignupSet(viewsets.ModelViewSet):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class signUpTraining(viewsets.ModelViewSet):
     queryset = Enrollments.objects.all()
     serializer_class = EnrollmentsSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['username']
-    
+
     @action(detail=False, methods=['post'])
     def signupfortraining(self, request, *args, **kwargs):
         userTrainingObjects = Enrollments.objects.filter(username=request.data["username"])
         userTrainingIDs = [i.training_id.training_id for i in userTrainingObjects]
         if int(request.data["training_id"]) in userTrainingIDs:
-            return Response({'error': 'User is already registered for this training!'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'User is already registered for this training!'},
+                            status=status.HTTP_400_BAD_REQUEST)
 
         catch_training = Training.objects.get(pk=request.data["training_id"])
         if catch_training.current_capacity >= catch_training.max_capacity:
-            return Response({'error': 'Capacity for the training session is full, please reach out to a health club employee for further information'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({
+                'error': 'Capacity for the training session is full, please reach out to a health club employee for further information'},
+                status=status.HTTP_400_BAD_REQUEST)
         catch_training.current_capacity += 1
         catch_training.save()
 
@@ -107,12 +112,14 @@ class signUpTraining(viewsets.ModelViewSet):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-    
+
+
 class viewTraining(viewsets.ModelViewSet):
     queryset = Training.objects.all()
     serializer_class = TrainingSerializer
-    permission_classes = [AllowAny] 
+    permission_classes = [AllowAny]
     lookup_field = 'pk'
+
     @action(detail=False, methods=['get'])
     def viewtrainingdetails(self, request, pk=None):
         if pk is not None:
@@ -124,11 +131,13 @@ class viewTraining(viewsets.ModelViewSet):
             serializer = self.get_serializer(trainings, many=True)
             return Response(serializer.data)
 
+
 class TokenRevokeSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['delete'])
     def revoke(self, request):
         request.auth.delete()
-        return Response({'Logged out successdfully!'},status=status.HTTP_200_OK)
+        return Response({'Logged out successdfully!'}, status=status.HTTP_200_OK)
+
 
 class CustomAuthToken(ObtainAuthToken):
     queryset = User.objects.all()
@@ -140,9 +149,34 @@ class CustomAuthToken(ObtainAuthToken):
         user = serializer.validated_data['user']
         nUser = User.objects.filter(username=user.username).get()
         if nUser.isNonMember():
-            return Response({"Non-members can't login"},status=status.HTTP_403_FORBIDDEN)
+            return Response({"Non-members can't login"}, status=status.HTTP_403_FORBIDDEN)
         token, created = Token.objects.get_or_create(user=user)
         return Response({
             'token': token.key,
             'first_name': user.first_name,
         })
+
+
+class LocationList(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    permission_classes = [AllowAny]
+
+    @action(detail=False, methods=['get'])
+    def locations(self, request):
+        locations = self.get_queryset()
+        serializer = self.get_serializer(locations, many=True)
+        return Response(serializer.data)
+
+
+class LocationDetails(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    permission_classes = [AllowAny]
+
+    @action(detail=False, methods=['get'])
+    def location_details(self, request):
+        location_details = self.filter_queryset(self.get_queryset())
+        location_details = location_details.values('location_name', 'location_address')
+        serializer = self.get_serializer(location_details, many=True)
+        return Response(serializer.data)
