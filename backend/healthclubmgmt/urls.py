a8c0a18e567bcfa587b3f713e7ab85ad7588e94a
darshcloud <darshini.vn@gmail.com>
2023-04-09 00:40:02 -0700

    Updated comments for API endpoints

--- backend/healthclubmgmt/urls.py
+++ backend/healthclubmgmt/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from . import views
-from .views import ClassSchedulesListView, SignupSet, signUpTraining, viewTraining, cancelEnrollment,ActivityLogView
+from .views import ClassSchedulesListView, SignupSet, signUpTraining, viewTraining, cancelEnrollment, ActivityLogView
 # router = DefaultRouter()
 # router.register(r'', UserLogViewSet)
-from .views import ClassSchedulesListView, UserLogViewSet, UserViewSet, SignupSet, TokenRevokeSet, CustomAuthToken, LocationList, LocationDetails, ViewMemberTrainingEnrollment
+from .views import ClassSchedulesListView, UserLogViewSet, UserViewSet, SignupSet, TokenRevokeSet, CustomAuthToken, \
+    LocationList, LocationDetails, ViewMemberTrainingEnrollment
 
 urlpatterns = [
     path('api/addClassSchedules/', ClassSchedulesListView.as_view()),
@@ -18,13 +19,15 @@ urlpatterns = [
     path('api/viewtrainings/<int:pk>', viewTraining.as_view({'get': 'viewtrainingdetails'})),
     path('login/', CustomAuthToken.as_view(), name='login'),
     path('logout/', TokenRevokeSet.as_view({'delete': 'revoke'})),
+    # API endpoint for get all location details including location id
     path('api/locations/', LocationList.as_view({'get': 'locations'})),
+    # API endpoint for get location name and location address
     path('api/locationdetails/', LocationDetails.as_view({'get': 'location_details'})),
-    #url endpoint to cancel enrollments for trainings--<int:pk> below is the training id--
+    # url endpoint to cancel enrollments for trainings--<int:pk> below is the training id--
     path('api/cancelenrollment/<int:pk>', cancelEnrollment.as_view({'delete': 'destroy'})),
     path('api/logHours/', ActivityLogView.as_view({'post': 'create'})),
 
     # API endpoint for view member specific training enrollment details
     path('api/viewmembertrainingenrollment/<int:user_id>', ViewMemberTrainingEnrollment.as_view({'get': 'list'}))
-    
+
 ]
