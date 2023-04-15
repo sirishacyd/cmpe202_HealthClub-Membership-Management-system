30228ca08f358d1b572f122c400ecce65a48c4a5
Bhavya Hegde <85700971+hegdebhavya@users.noreply.github.com>
2023-04-15 10:13:43 -0700

    drop expired trainings from frontend

--- backend/healthclubmgmt/apps.py
+++ backend/healthclubmgmt/apps.py
@@ -6,11 +6,6 @@ class HealthclubmgmtConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'healthclubmgmt'
     
-    def ready(self):
-        from healthclubmgmt.models import Training
-        now = timezone.now()
-        old_trainings = Training.objects.filter(end_time__lt=now)
-        old_trainings.delete()
  
     
     
