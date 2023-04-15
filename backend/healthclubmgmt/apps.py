b5dee53f3853ab0e1a8e814b6a26511e0c7938a0
Bhavya Hegde <85700971+hegdebhavya@users.noreply.github.com>
2023-04-15 10:01:45 -0700

    Retire expired trainings

--- backend/healthclubmgmt/apps.py
+++ backend/healthclubmgmt/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
+from django.utils import timezone
 
 
 class HealthclubmgmtConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'healthclubmgmt'
+    
+    def ready(self):
+        from healthclubmgmt.models import Training
+        now = timezone.now()
+        old_trainings = Training.objects.filter(end_time__lt=now)
+        old_trainings.delete()
+ 
+    
+    
