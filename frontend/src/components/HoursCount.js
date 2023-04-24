d966cf57196816161fb99d6e2b0b84cb3fb65962
sirishacyd <sirishacyd@gmail.com>
2023-04-24 00:52:58 -0500

    align fix for hourscount

--- frontend/src/components/HoursCount.js
+++ frontend/src/components/HoursCount.js
@@ -88,14 +88,15 @@ function HoursCount({ locationId }) {
 
   return (
     <div className="HoursCount" style={{ width: "60%", margin: "0 auto" }}>
+    <p style={{ fontSize: "18px" }} align="center">Hours spent in the gym by day/week/month</p>
       <Form.Control
         as="select"
         value={selectedChart}
         onChange={(e) => setSelectedChart(e.target.value)}
       >
-        <option value="daily">Daily Hours</option>
-        <option value="weekly">Weekly Hours</option>
-        <option value="monthly">Monthly Hours</option>
+        <option value="daily">Daily</option>
+        <option value="weekly">Weekly</option>
+        <option value="monthly">Monthly</option>
       </Form.Control>
 
       <Bar data={chartData[selectedChart]} />
